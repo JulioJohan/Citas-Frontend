cc7e4538a9878b6137e76ae7d0f2b2b6c748e71d
Karla <karladk038@gmail.com>
2023-10-15 02:10:05 -0600

    Agregue diseño

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -5,7 +5,7 @@ export const defaultNavigation: FuseNavigationItem[] = [
     {
         id      : 'dashboards',
         title   : 'Dashboards',
-        subtitle: 'Unique dashboard designs',
+        // subtitle: 'Dashboard designado',
         type    : 'group',
         icon    : 'heroicons_outline:home',
         children: [
