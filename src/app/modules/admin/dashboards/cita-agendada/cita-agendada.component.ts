860b136b823f1eda28db808e1feaa5c19d22a4b1
Karla <karladk038@gmail.com>
2023-10-15 01:39:51 -0600

    Se crearon componentes de registro doctor

---
+++ src/app/modules/admin/dashboards/cita-agendada/cita-agendada.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cita-agendada',
+  templateUrl: './cita-agendada.component.html',
+  styleUrls: ['./cita-agendada.component.scss']
+})
+export class CitaAgendadaComponent {
+
+}
---
+++ src/app/modules/admin/dashboards/resgistro-medico/resgistro-medico.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resgistro-medico',
+  templateUrl: './resgistro-medico.component.html',
+  styleUrls: ['./resgistro-medico.component.scss']
+})
+export class ResgistroMedicoComponent {
+
+}
