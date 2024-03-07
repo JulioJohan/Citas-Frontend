04d2edc97eef75a0e787fd5caf1cc931e8844e8e
Karla038 <karladk038@gmail.com>
2024-03-07 14:57:57 -0600

    Diseño en conoce+

--- src/app/modules/home/conoce-mas/conoce-mas.component.ts
+++ src/app/modules/home/conoce-mas/conoce-mas.component.ts
@@ -10,4 +10,21 @@ export class ConoceMasComponent {
 
     currentRouteHome = '/home-nuevo'
     currentRouteConoce = '/conoce-mas'
+
+    navbarOpen = false;
+    movile = window.innerWidth < 1024;
+
+    toggleNavbar() {
+        this.navbarOpen = !this.navbarOpen;
+    }
+
+    ngOnInit() {
+        // Escuchar cambios de tamaño de pantalla para ajustar la visibilidad del navbar
+        window.addEventListener('resize', () => {
+          this.movile = window.innerWidth < 1024;
+          if (!this.movile) {
+            this.navbarOpen = false;
+          }
+        });
+    }
 }
