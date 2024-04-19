af6c19467a9aac0c2769f62939ca51458b3e8497
Karla038 <karladk038@gmail.com>
2024-04-18 23:45:57 -0600

    Descomentar los enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
     production: false,
 
-    // apiUrl: 'https://aphospital.onrender.com',
+    apiUrl: 'https://aphospital.onrender.com',
     // servLogin: "https://18.220.73.90/backendlogin/api"
 
-    apiUrl: 'http://localhost:4000',
+    // apiUrl: 'http://localhost:4000',
 
     recaptcha: {
         captchaId: '6LcGmr8kAAAAAHXgMigTHdTGGbmWR8F-KJok4OKV',
