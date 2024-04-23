8acab3fc3299bec16120f127378a8e1e27d12c0b
Johan Mejia <x374019@gruposantander.com>
2024-04-22 23:04:41 -0600

    add console.log

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -146,7 +146,7 @@ export class AuthSignInComponent implements OnInit, AfterViewInit
                 })
                 this._router.navigateByUrl('/users/two-authentication');
         
-              },3000)
+              },6000)
         },
         (error:any) =>{
             this.errorRespuesta(error.error.msg)
